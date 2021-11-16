5ecedb6f730b59f9bfdb0e62464f06b3f23bdb4a
胖虎 <mygithu@126.com>
2021-11-16 18:40:28 +0800

    init: 初始化 自定义作业

---
+++ src/views/practice/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>自定义指令</h3>
+    <div>
+      <input v-model="value" v-anti-shake="onShowInfo" type="text">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onShowInfo() {
+      console.log(this.value)
+    }
+  }
+}
+</script>
+
+<style></style>
